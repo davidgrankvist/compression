2844d6952ab774ae71490f05351b79c77e877e81
David Grankvist <grankvist.david@gmail.com>
2024-08-30 22:19:11 +0200

    add RLE decoder

--- Compression.Test/RunLengthEncoderTest.cs
+++ Compression.Test/RunLengthEncoderTest.cs
@@ -28,5 +28,53 @@ namespace Compression.Test
 
 			PipelineTestHelpers.CheckPipeline(pipeline, input, expected);
 		}
+
+		[TestMethod]
+		public void ShouldDecodeRepetitions()
+		{
+			var input = new byte[] { 3, 1, 3, 2, 3, 3 };
+			var expected = new byte[] { 1, 1, 1, 2, 2, 2, 3, 3, 3 };
+
+			var pipeline = new EncoderPipeline(new RunLengthDecoder());
+
+			PipelineTestHelpers.CheckPipeline(pipeline, input, expected);
+		}
+
+		[TestMethod]
+		public void ShouldDecodeUnique()
+		{
+			var input = new byte[] { 1, 2, 1, 3, 1, 4 };
+			var expected = new byte[] { 2, 3, 4 };
+
+			var pipeline = new EncoderPipeline(new RunLengthDecoder());
+
+			PipelineTestHelpers.CheckPipeline(pipeline, input, expected);
+		}
+
+		[TestMethod]
+		public void ShouldEncodeDecodeRepetitions()
+		{
+			var input = new byte[] { 1, 1, 1, 2, 2, 2, 3, 3, 3 };
+
+			var pipeline = new EncoderPipelineBuilder()
+				.Add(new RunLengthEncoder())
+				.Add(new RunLengthDecoder())
+				.Build();
+
+			PipelineTestHelpers.CheckPipeline(pipeline, input, input);
+		}
+
+		[TestMethod]
+		public void ShouldEncodeDecodeUnique()
+		{
+			var input = new byte[] { 2, 3, 4 };
+
+			var pipeline = new EncoderPipelineBuilder()
+				.Add(new RunLengthEncoder())
+				.Add(new RunLengthDecoder())
+				.Build();
+
+			PipelineTestHelpers.CheckPipeline(pipeline, input, input);
+		}
 	}
 }
